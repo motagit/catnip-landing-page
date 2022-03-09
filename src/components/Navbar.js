bfcc0d5cf2d2d253acabc9febacfcf5c7c71b377
Lucas Lopes <lucas.lopes@kron.digital>
2022-03-09 18:59:25 -0300

    button component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,78 @@
-import React, { useState } from 'react'
-import logo from '../assets/logo.png'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import logo from "../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
       <div className="navbar">
-          <div className="navbar-container">
-              {/* scrollLink  */}
-              <div className="navbar-logo">
-                  <img src={logo} alt="" />
-                  <h1>teste</h1>
-              </div>
-              <div className="menu-icon" onClick={handleClick}>
-                {click ? <FaTimes /> : <FaBars />}
-              </div>
-              <ul className={click ? 'nav-menu active' : 'nav-menu'}>   
-                <li className="nav-item">
-                  {/* scrollLink */}
-                  <div className="nav-links">
-                    ABOUT US
-                  </div>
-                </li>
-                <li className="nav-item">
-                  {/* scrollLink */}
-                  <div className="nav-links">
-                    ADOPTION
-                  </div>
-                </li>
-                <li className="nav-item">
-                  {/* scrollLink */}
-                  <div className="nav-links">
-                    HELP US
-                  </div>
-                </li>
-                <li className="nav-item">
-                  {/* scrollLink */}
-                  <div className="nav-links">
-                    Contact
-                  </div>
-                </li>
-                <li className="nav-btn">
-                  {button ? (
-                    <a href='#' target="_blank" rel="noopener noreferrer" className="btn-link">
-                      <Button buttonStyle='btn--outline'>DONATE</Button>
-                      DONATE
-                    </a>
-                    ) : (
-                    
-                    <a href='#' target="_blank" rel="noopener noreferrer" className="btn-link">
-                      <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>DONATE</Button>
-                    </a>
-                   )} 
-                </li>
-
-              </ul>
+        <div className="navbar-container">
+          {/* scrollLink  */}
+          <div className="navbar-logo">
+            <img src={logo} alt="" />
           </div>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              {/* scrollLink */}
+              <div className="nav-links">ABOUT US</div>
+            </li>
+            <li className="nav-item">
+              {/* scrollLink */}
+              <div className="nav-links">ADOPTION</div>
+            </li>
+            <li className="nav-item">
+              {/* scrollLink */}
+              <div className="nav-links">HELP US</div>
+            </li>
+            <li className="nav-item">
+              {/* scrollLink */}
+              <div className="nav-links">CONTACT</div>
+            </li>
+            <li className='nav-btn'>
+                {button ? (
+                  <Link to='/sign-up' className='btn-link'>
+                    <Button buttonStyle='btn--outline' buttonColor='primary'>DONATE</Button>
+                  </Link>
+                ) : (
+                  <Link to='/sign-up' className='btn-link'>
+                    <Button
+                      buttonStyle='btn--medium'
+                      
+                      buttonSize='btn--mobile'
+                      onClick={closeMobileMenu}
+                    >
+                      DONATE
+                    </Button>
+                  </Link>
+                )}
+              </li>
+          </ul>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
